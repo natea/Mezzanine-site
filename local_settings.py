b75cb981b607aa796453a3a02dd8f636b1459006
Nate Aune <natea@jazkarta.com>
2010-11-19 20:14:36 -0500

    make sure that the filebrowser upload dir is in site_media

--- local_settings.py
+++ local_settings.py
@@ -9,3 +9,8 @@ DATABASES = {
     }
 }
 
+if PACKAGE_NAME_FILEBROWSER in INSTALLED_APPS:
+    FILEBROWSER_URL_FILEBROWSER_MEDIA = "/site_media/filebrowser"
+    FILEBROWSER_PATH_FILEBROWSER_MEDIA = os.path.join(
+              _package_path(PACKAGE_NAME_FILEBROWSER), "site_media", "filebrowser")
+
